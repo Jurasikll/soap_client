48e284978b56217296f27fe5b22383657ad25d7f
Jurasikll <Jurasikll@yandex.ru>
2017-09-27 13:39:03 +0300

    Initial commit

---
+++ doc.go
@@ -0,0 +1,6 @@
+// soap_client project doc.go
+
+/*
+soap_client document
+*/
+package main
---
+++ main.go
@@ -0,0 +1,10 @@
+// soap_client project main.go
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello World!")
+}
